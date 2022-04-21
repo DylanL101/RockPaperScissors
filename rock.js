a6cdc3209d96d3e8803a891b022acb295df408a8
Dylan <dylblob@gmail.com>
2022-04-21 00:22:16 -0400

    Add function logMatch and logWin To keep track of the number of matchs wons and the number of wins for the computer or the player.

--- rock.js
+++ rock.js
@@ -1,18 +1,24 @@
-const options =["ROCK", "PAPER", "SCISSORS"];
+const options =["ROCK", "PAPER", "SCISSORS"]
+const winner = []
 function computerOption(){
     return options[Math.floor(Math.random()*options.length)]
     
 }
-console.log(options[Math.floor(Math.random()*options.length)]);
+
     function game(){
-        playRound();
+        for (let i= 0; i<= 5; i++){
+            playRound(i);
+        }
+        logWins();
     }
     
-    function playRound(){
-    const playerSeletion = playerOption();
+    function playRound(match){
+    const playerSelection = playerOption();
     const computerSelection = computerOption();
 
-  
+    const win = winCheck(playerSelection, computerSelection);
+    winner.push(win);
+    logMatch(playerSelection,computerSelection, win, match);
     }
     
     function playerOption(){
@@ -20,32 +26,47 @@ console.log(options[Math.floor(Math.random()*options.length)]);
         while (answer == null){
             answer = prompt("Choose either Rock, Paper, or Scissors");
             }
-            answer = answer.toUpperCase();
-        }
+        answer = answer.toUpperCase();
         let check = validateAnswer(answer)
-        while (check == false)
+        while (check == false){
             answer = prompt(" Please choose either Rock, Paper, or Scissors. Make sure your spelling is correct.");
         while (answer == null){
-            anwer = prompt ("Choose either Rock, Paper, or Scissors");
+            answer = prompt ("Choose either Rock, Paper, or Scissors");
         }
-        answer = answer.toUpperCase();
+        answer = answer.toUpperCase()
         check = validateAnswer(answer);
-
+    }
+        return answer;
+}
     function  validateAnswer(option){
-        if(options.includes(option)){
-            return true
-        } else {
-            return false
-        }
+        return options.includes(option);
+           
     }
     function winCheck (optionP, optionC){
         if (optionP === optionC){
             return "Tie game";
-     } else if (optionP === "PAPER" && optionC === "ROCK" || optionP === "ROCK" && optionC === "SCISSORS" || optionP === "SCISSORS" && optionC === "PAPER")
+     } else if ((optionP === "PAPER" && optionC === "ROCK") || (optionP === "ROCK" && optionC === "SCISSORS") || (optionP === "SCISSORS" && optionC === "PAPER")){
         return "Winner!Gagneau! Player has won!";
-        else{
-            return "Computer has won!"
-        }
-        }
+     } else{
+            return "Computer has won!";
+     }
+ }
+ function logWins(){
+    let playerWins = winner.filter((item) => item == "Player").length;
+    let computerWins = winner.filter((item) => item == "Computer").length;
+    let tie = winner.filter((item) => item == "Ties").length;
+    console.log("Result");
+    console.log("Player Wins:", playerWins);
+    console.log("Computer Wins:", computerWins);
+    console.log("Ties:", tie);
+}
 
-    game();
+    function logMatch(playerOption,computerOption, win,match){
+        console.log("Match:", match);
+        console.log("Player Option", playerOption);
+        console.log("Computer Option", computerOption);
+        console.log( win, "Won the Match");
+        
+    }
+
+game();
