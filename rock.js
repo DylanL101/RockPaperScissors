4c1da3f0463f97038cc86bf8e5eabdd3569dcbb5
Dylan <dylblob@gmail.com>
2022-04-20 22:39:42 -0400

    Add the variables answer and check to ensure that the player Does not crash the game when there is an incorrect input or a spelling mistake.

--- rock.js
+++ rock.js
@@ -1,21 +1,38 @@
 const options =["ROCK", "PAPER", "SCISSORS"];
-function computerPlay(){
+function computerOption(){
     return options[Math.floor(Math.random()*options.length)]
     
 }
-    console.log(options[Math.floor(Math.random()*options.length)]);
+console.log(options[Math.floor(Math.random()*options.length)]);
+    function game(){
+        playRound();
+    }
     
     function playRound(){
-     const playerSelection = playerOption();
-     const computerSelection = computerOption();
+    const playerSeletion = playerOption();
+    const computerSelection = computerOption();
+
+  
     }
     
     function playerOption(){
         let answer = prompt("Choose either Rock, Paper, or Scissors");
         while (answer == null){
-        answer = prompt("Choose either Rock, Paper, or Scissors");
+            answer = prompt("Choose either Rock, Paper, or Scissors");
+            }
+            answer = answer.toUpperCase();
         }
-
-
-
-game();
+        let check = validateAnswer(answer)
+        while (check == false)
+            answer = prompt(" Please choose either Rock, Paper, or Scissors. Make sure your spelling is correct.");
+        while (answer == null){
+            anwer = prompt ("Choose either Rock, Paper, or Scissors");
+        }
+    function  validateAnswer(option){
+        if(options.includes(option)){
+            return true
+        } else {
+            return false
+        }
+    }
+    game();
