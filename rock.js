d8ace5929226246c0925fe87134bdf46c1997adf
Dylan <dylblob@gmail.com>
2022-04-20 21:28:23 -0400

    Create function playRound to contain parameters The two parameters that are contained in the function playRound are constants playerSelection and computerSelection, they are equal to function playerOption and computerOption. Also, change the name of the constant playerOption to options to avoid less confusion with the function playerOption.

--- rock.js
+++ rock.js
@@ -1,6 +1,21 @@
-const playOption =["ROCK", "PAPER", "SCISSORS"];
+const options =["ROCK", "PAPER", "SCISSORS"];
 function computerPlay(){
-    return playOption[Math.floor(Math.random()*playOption.length)]
+    return options[Math.floor(Math.random()*options.length)]
     
 }
-    console.log(playOption[Math.floor(Math.random()*playOption.length)]);
+    console.log(options[Math.floor(Math.random()*options.length)]);
+    
+    function playRound(){
+     const playerSelection = playerOption();
+     const computerSelection = computerOption();
+    }
+    
+    function playerOption(){
+        let answer = prompt("Choose either Rock, Paper, or Scissors");
+        while (answer == null){
+        answer = prompt("Choose either Rock, Paper, or Scissors");
+        }
+
+
+
+game();
