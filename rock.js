e2ba1b33c1d46c2984987143cd76db7f373ab0ca
Dylan <dylblob@gmail.com>
2022-04-20 21:10:23 -0400

    Create function playOption, computer chooses 'Rock', 'Paper, 'Scissors' Return the output in the console

---
+++ rock.js
@@ -0,0 +1,6 @@
+const playOption =["ROCK", "PAPER", "SCISSORS"];
+function computerPlay(){
+    return playOption[Math.floor(Math.random()*playOption.length)]
+    
+}
+    console.log(playOption[Math.floor(Math.random()*playOption.length)]);
